5d81cf7d3732fd11ce0ed0a4a345bb52eb881fd7
Viktorija Filipov <viktorijapoe@gmail.com>
2014-11-24 13:30:10 +0100

    Update relationships_controller_spec.rb

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require 'spec_helper'
 
 describe RelationshipsController do
@@ -40,47 +39,4 @@ describe RelationshipsController do
       expect(response).to be_success
     end
   end
-=======
-require 'spec_helper'
-
-describe RelationshipsController do
-
-  let(:user) { FactoryGirl.create(:user) }
-  let(:other_user) { FactoryGirl.create(:user) }
-
-  before { sign_in user, no_capybara: true }
-
-  describe "creating a relationship with Ajax" do
-
-    it "should increment the Relationship count" do
-      expect do
-        xhr :post, :create, relationship: { followed_id: other_user.id }
-      end.to change(Relationship, :count).by(1)
-    end
-
-    it "should respond with success" do
-      xhr :post, :create, relationship: { followed_id: other_user.id }
-      expect(response).to be_success
-    end
-  end
-
-  describe "destroying a relationship with Ajax" do
-
-    before { user.follow!(other_user) }
-    let(:relationship) do
-      user.relationships.find_by(followed_id: other_user.id)
-    end
-
-    it "should decrement the Relationship count" do
-      expect do
-        xhr :delete, :destroy, id: relationship.id
-      end.to change(Relationship, :count).by(-1)
-    end
-
-    it "should respond with success" do
-      xhr :delete, :destroy, id: relationship.id
-      expect(response).to be_success
-    end
-  end
->>>>>>> 7dfc48e6a4cfc7ee922a09d3afb1d21ae7ecb985
 end
