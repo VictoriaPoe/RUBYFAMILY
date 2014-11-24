6595deb4b57fe6615f5add23657c1268da923b43
Viktorija Filipov <viktorijapoe@gmail.com>
2014-11-24 13:32:42 +0100

    Update utilities.rb

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def full_title(page_title)
   base_title = "Ruby on Rails Tutorial Sample App"
   if page_title.empty?
@@ -20,27 +19,4 @@ def sign_in(user, options={})
     fill_in "Password", with: user.password
     click_button "Sign in"
   end
-=======
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
-
-def sign_in(user, options={})
-  if options[:no_capybara]
-    # Sign in when not using Capybara.
-    remember_token = User.new_remember_token
-    cookies[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.digest(remember_token))
-  else
-    visit signin_path
-    fill_in "Email",    with: user.email
-    fill_in "Password", with: user.password
-    click_button "Sign in"
-  end
->>>>>>> 7dfc48e6a4cfc7ee922a09d3afb1d21ae7ecb985
 end
